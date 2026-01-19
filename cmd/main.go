a68169d717e536fb6f25c145f601fb2c8a4d83bf
qweviluxx <kovalfw@gmail.com>
2026-01-19 21:05:51 +0300

    ScanPort goroutines (before worker pool)

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/qweviluxx/GopherScanner.git/internal"
 )
 
 func main() {
-	conn, err := internal.ScanPort("tcp", "scanme.nmap.org", 80)
+	s := internal.NewScanner("tcp")
+	conn, err := s.ScanPort("scanme.nmap.org", 80)
 	fmt.Println(conn)
 	fmt.Println(err)
+
+	start := time.Now()
+	s.ScanRange("scanme.nmap.org", 20, 100)
+	duration := time.Since(start)
+	fmt.Println(duration)
 }
--- internal/scanner.go
+++ internal/scanner.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -18,16 +19,33 @@ func NewScanner(protocol string) *Scanner {
 	}
 }
 
-var timeout time.Duration = 1 * time.Second
+var timeout time.Duration = 500 * time.Millisecond
 
-func ScanPort(protocol, hostname string, port int) (bool, error) {
+func (s *Scanner) ScanPort(hostname string, port int) (bool, error) {
 	address := hostname + fmt.Sprintf(":%d", port)
-	conn, err := net.DialTimeout(protocol, address, timeout)
+	conn, err := net.DialTimeout(s.Protocol, address, s.timeout)
 
 	if err != nil {
-		fmt.Printf("TCP connection error: %v", err)
 		return false, err
 	}
 	conn.Close()
 	return true, nil
+}
+
+func (s *Scanner) ScanRange(hostname string, startPort, endPort int) {
+	var wg sync.WaitGroup
+	for i := startPort; i <= endPort; i++ {
+		wg.Add(1)
+
+		go func(p int) {
+			defer wg.Done()
+			ok, _ := s.ScanPort(hostname, p)
+			if ok {
+				fmt.Println("Port", p, "is open")
+			}
+		}(i)
+
+	}
+
+	wg.Wait()
 }
